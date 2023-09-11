42f5ee3b54e9eb125c3c11e32848ca14366497a6
Ricky Wang <wangr@midlandps.org>
2023-09-10 23:40:03 -0400

    Create TeleOpTest1.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest1.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class TeleOpTest1 extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addData("Status:", "Doing fine :)");
+
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Run Status:", "Im running, I think...");
+    }
+}
