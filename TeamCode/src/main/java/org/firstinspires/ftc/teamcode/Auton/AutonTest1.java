fd865a3591383df9592dfe31aea384b7641aa1dc
Ricky Wang <wangr@midlandps.org>
2023-09-16 16:29:39 -0400

    Added Roadrunner

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/AutonTest1.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.Auton;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class AutonTest1 extends LinearOpMode {
+    @Override
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+
+
+
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
